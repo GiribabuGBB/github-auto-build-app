97e36fb8676eeaabc0f19ea52432a90745896706
GiribabuGBB <139129775+GiribabuGBB@users.noreply.github.com>
2025-08-05 14:59:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello, GitHub Actions!');
+  res.send('Hello, GitHub Actions!Mgg');
 });
 
 const PORT = process.env.PORT || 3000;
